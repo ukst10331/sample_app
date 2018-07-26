9e599b062f4111f119cdec0eb75eb4ae49bf1971
EC2 Default User <ec2-user@ip-172-31-18-213.ec2.internal>
2018-07-26 11:09:48 +0000

    Make a Micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+  has_many :microposts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
