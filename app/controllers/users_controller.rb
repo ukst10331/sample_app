cb9a2a7f5b53a6c1b18d93ce5dd4dd7bc010cc54
EC2 Default User <ec2-user@ip-172-31-18-213.ec2.internal>
2018-08-03 02:18:20 +0000

    Finish user follow / unfollow actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :except=>[:show]
   before_action :admin_user,     only: :destroy
-
+  
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
@@ -27,7 +27,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private
 
     def admin_user
